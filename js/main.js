55d3ff06d25d6c4482fe816507f9b031aaf2b991
Frank Bishop <ffbishop3@gmail.com>
2021-02-08 15:13:52 -0800

    Listen for 'submit' events on the journal entry form and:

--- js/main.js
+++ js/main.js
@@ -4,7 +4,12 @@
 var imageField = document.getElementById('image-URL');
 var imageChange = document.querySelector('.image-to-change');
 imageField.addEventListener('input', changeImage);
+// var submitButton = document.getElementById('Save');
+// submitButton.addEventListener('submit', submitAction);
 
 function changeImage(event) {
   imageChange.setAttribute('src', imageField.value);
 }
+// function submitAction(event) {
+
+// }
