ee72ce8f12921c063786bb874c5308eb3d9a2115
Frank Bishop <ffbishop3@gmail.com>
2021-02-08 15:49:35 -0800

    Add the nextEntryId to the object.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,24 @@
 /* exported data */
 
 var imageField = document.getElementById('image-URL');
+var titleField = document.getElementById('Title');
+var notesField = document.getElementById('Notes');
 var imageChange = document.querySelector('.image-to-change');
 imageField.addEventListener('input', changeImage);
-// var submitButton = document.getElementById('Save');
-// submitButton.addEventListener('submit', submitAction);
-
+// var entryID = 0
+var nextEntryId = 1;
+var formField = document.querySelector('form');
+formField.addEventListener('submit', submitAction);
 function changeImage(event) {
   imageChange.setAttribute('src', imageField.value);
 }
-// function submitAction(event) {
-
-// }
+function submitAction(event) {
+  event.preventDefault();
+  var inputs = {
+    image: imageField.value,
+    title: titleField.value,
+    notes: notesField.value
+  };
+  inputs.nextEntryId = nextEntryId;
+  // console.log(inputs)
+}
