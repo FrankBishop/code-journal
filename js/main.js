c54e36066c3b0625f29634e57849455927e57d65
Frank Bishop <ffbishop3@gmail.com>
2021-02-08 19:25:49 -0800

    fixed issue 1

--- js/main.js
+++ js/main.js
@@ -2,13 +2,12 @@
 /* exported data */
 
 var imageField = document.querySelector('#image-URL');
-var titleField = document.querySelector('#Title');
-var notesField = document.querySelector('#Notes');
+var titleField = document.querySelector('#title');
+var notesField = document.querySelector('#notes');
 var imageChange = document.querySelector('.image-to-change');
-imageField.addEventListener('input', changeImage);
 var entryID = 0;
-var nextEntryId = 1;
 var formField = document.querySelector('form');
+imageField.addEventListener('input', changeImage);
 formField.addEventListener('submit', submitAction);
 function changeImage(event) {
   imageChange.setAttribute('src', imageField.value);
@@ -21,9 +20,9 @@ function submitAction(event) {
     notes: notesField.value
   };
   inputs.entryId = entryID++;
-  inputs.nextEntryId = nextEntryId++;
-  data.nextEntryId = data.nextEntryId++;
-  data.entries.push(inputs);
+  inputs.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(inputs);
   imageChange.setAttribute('src', 'images/placeholder-image-square.jpg');
   formField.reset();
   var storedData = JSON.stringify(data);
