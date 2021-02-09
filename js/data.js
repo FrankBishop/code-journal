9c7df8d8b36ca63e7188b05df2c6fcf43d50356d
Frank Bishop <ffbishop3@gmail.com>
2021-02-09 15:29:27 -0800

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/data.js
+++ js/data.js
@@ -7,6 +7,9 @@ var data = {
   nextEntryId: 1
 };
 
+// var entries = document.querySelector('.entries');
+var list = document.querySelector('ul');
+
 window.addEventListener('load', storedDataLoad);
 
 window.addEventListener('beforeunload', localStorageSet);
@@ -21,4 +24,26 @@ function storedDataLoad(event) {
   if (previousData !== null) {
     data = JSON.parse(previousData);
   }
+}
+addJournal();
+
+function addJournal(entry) {
+  var listElement = document.createElement('li');
+  list.appendChild(listElement);
+  var entryRow = document.createElement('div');
+  listElement.appendChild(entryRow);
+  entryRow.className = 'row';
+  var entryImage = document.createElement('img');
+  entryRow.appendChild(entryImage);
+  entryImage.className = 'column-half';
+  entryImage.setAttribute('src', data.entries[0].image);
+  var entryMain = document.createElement('div');
+  entryRow.appendChild(entryMain);
+  entryMain.className = 'column-half';
+  var entryHeader = document.createElement('h1');
+  entryMain.appendChild(entryHeader);
+  entryHeader.textContent = data.entries[0].title;
+  var entryText = document.createElement('p');
+  entryMain.appendChild(entryText);
+  entryText.textContent = data.entries[0].notes;
 }
