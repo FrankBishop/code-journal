2b3a721450c96fa573ea1ddf7c4d5511eb67e3a4
Frank Bishop <ffbishop3@gmail.com>
2021-02-09 12:37:40 -0800

    implement requested changes and moved local storage code to data.js

--- js/data.js
+++ js/data.js
@@ -6,3 +6,19 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('load', storedDataLoad);
+
+window.addEventListener('beforeunload', localStorageSet);
+
+function localStorageSet(event) {
+  var storedData = JSON.stringify(data);
+  localStorage.setItem('code-journal-data', storedData);
+}
+
+function storedDataLoad(event) {
+  var previousData = localStorage.getItem('code-journal-data');
+  if (previousData !== null) {
+    data = JSON.parse(previousData);
+  }
+}
--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ var imageField = document.querySelector('#image-URL');
 var titleField = document.querySelector('#title');
 var notesField = document.querySelector('#notes');
 var imageChange = document.querySelector('.image-to-change');
-var entryID = 0;
 var formField = document.querySelector('form');
 imageField.addEventListener('input', changeImage);
 formField.addEventListener('submit', submitAction);
@@ -19,12 +18,9 @@ function submitAction(event) {
     title: titleField.value,
     notes: notesField.value
   };
-  inputs.entryId = entryID++;
-  inputs.nextEntryId = data.nextEntryId;
+  inputs.entryId = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift(inputs);
   imageChange.setAttribute('src', 'images/placeholder-image-square.jpg');
   formField.reset();
-  var storedData = JSON.stringify(data);
-  localStorage.setItem('Coding Notes Entries', storedData);
 }
