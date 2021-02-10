19593a818d5123cfdca9fbe9ae1032d50900b036
Frank Bishop <ffbishop3@gmail.com>
2021-02-09 19:28:42 -0800

    some of Issue 2 restructuring

--- js/data.js
+++ js/data.js
@@ -7,11 +7,6 @@ var data = {
   nextEntryId: 1
 };
 
-var list = document.querySelector('ul');
-var i = 0;
-
-window.addEventListener('DOMContentLoaded', addPastJournals);
-
 window.addEventListener('beforeunload', localStorageSet);
 
 var previousData = localStorage.getItem('code-journal-data');
@@ -22,31 +17,4 @@ if (previousData !== null) {
 function localStorageSet(event) {
   var storedData = JSON.stringify(data);
   localStorage.setItem('code-journal-data', storedData);
-}
-
-function addJournal(entry) {
-  var listElement = document.createElement('li');
-  list.appendChild(listElement);
-  var entryRow = document.createElement('div');
-  listElement.appendChild(entryRow);
-  entryRow.className = 'row';
-  var entryImage = document.createElement('img');
-  entryRow.appendChild(entryImage);
-  entryImage.className = 'column-half';
-  entryImage.setAttribute('src', data.entries[i].image);
-  var entryMain = document.createElement('div');
-  entryRow.appendChild(entryMain);
-  entryMain.className = 'column-half';
-  var entryHeader = document.createElement('h1');
-  entryMain.appendChild(entryHeader);
-  entryHeader.textContent = data.entries[i].title;
-  var entryText = document.createElement('p');
-  entryMain.appendChild(entryText);
-  entryText.textContent = data.entries[i].notes;
-}
-
-function addPastJournals(event) {
-  for (i = 0; i < data.entries.length - 1; i++) {
-    addJournal(data.entries[i]);
-  }
 }
--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ var titleField = document.querySelector('#title');
 var notesField = document.querySelector('#notes');
 var imageChange = document.querySelector('.image-to-change');
 var formField = document.querySelector('form');
+var list = document.querySelector('ul');
+var i = 0;
+window.addEventListener('DOMContentLoaded', addPastJournals);
 imageField.addEventListener('input', changeImage);
 formField.addEventListener('submit', submitAction);
 function changeImage(event) {
@@ -23,6 +26,32 @@ function submitAction(event) {
   data.entries.unshift(inputs);
   imageChange.setAttribute('src', 'images/placeholder-image-square.jpg');
   formField.reset();
-  location.reload();
+  addJournal();
   window.location.href = '#entries';
+}
+function addJournal(entry) {
+  var listElement = document.createElement('li');
+  list.appendChild(listElement);
+  var entryRow = document.createElement('div');
+  listElement.appendChild(entryRow);
+  entryRow.className = 'row';
+  var entryImage = document.createElement('img');
+  entryRow.appendChild(entryImage);
+  entryImage.className = 'column-half';
+  entryImage.setAttribute('src', data.entries[i].image);
+  var entryMain = document.createElement('div');
+  entryRow.appendChild(entryMain);
+  entryMain.className = 'column-half';
+  var entryHeader = document.createElement('h1');
+  entryMain.appendChild(entryHeader);
+  entryHeader.textContent = data.entries[i].title;
+  var entryText = document.createElement('p');
+  entryMain.appendChild(entryText);
+  entryText.textContent = data.entries[i].notes;
+}
+
+function addPastJournals(event) {
+  for (i = 0; i < data.entries.length - 1; i++) {
+    addJournal(data.entries[i]);
+  }
 }
